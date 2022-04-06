1baad94149bd0c4604e8a2743363045af6abc83f
mpds <matheusmullerps@gmail.com>
2022-04-06 17:04:29 -0300

    parametrize debug settings

--- cardabot_api/settings.py
+++ cardabot_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG_DEV"))
 
 ALLOWED_HOSTS = []
 
