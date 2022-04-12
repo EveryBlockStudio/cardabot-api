fd6d31ead4ea61f50d399eec54917fc3d1deea9d
Lincon Vidal <linconvidal@gmail.com>
2022-04-12 07:56:43 -0300

    fix requeriments and settings for gunicorn (prod deploy)

--- cardabot_api/settings.py
+++ cardabot_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG_DEV"))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
