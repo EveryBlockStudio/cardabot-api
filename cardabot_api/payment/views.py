4e0d82d9964588504ed1d55d7cc423d44f6de752
Lincon Vidal <linconvidal@gmail.com>
2022-06-22 18:05:13 -0300

    Add payment page and JS to sign and submit the tx

--- cardabot_api/payment/views.py
+++ cardabot_api/payment/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from ..cardabot.models import UnsignedTransaction
 
-def payment(request):
-    return render(request, 'base.html')
+def payment(request, tx_id):
+    #return render(request, 'payment.html')
+    #tx_id = request.GET.get("tx_id", "")
+    obj = get_object_or_404(Transaction, pk=tx_id)
+    context = {obj.__dict__.keys(): obj.__dict__.values()}
+    return render(request, 'payment.html', context)
