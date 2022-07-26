02e47620a86473620a7378a13ca4044857c02718
Lincon Vidal <lincon.vidal@everyblock.studio>
2022-07-26 15:19:51 -0300

    hot fix 404 for wallet connection page (no token)

--- cardabot_api/wallet_connection/views.py
+++ cardabot_api/wallet_connection/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from cardabot_api.cardabot.models import Chat
+from django.shortcuts import get_object_or_404
 
 def wallet_connection(request):
+    """ Wallet connection page """
+    if request.method == 'GET':
+        # get token from request
+        tmp_token = request.GET.get('token')
+        
+        # filter chat_id by tmp_token or return 404
+        chat_id = get_object_or_404(Chat, tmp_token=tmp_token)
+
     return render(request, 'connect.html')
 
 def wallet_connection_success(request):
